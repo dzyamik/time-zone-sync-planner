0f5efc62979b209df942397faefd3db76866a881
d.parubochyi <d.parubochy@gmail.com>
2025-06-08 09:45:39 +0300

    Adding correct deploy mechanism

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // Replace with your repo name
-const repo = 'timezone-sync'
+const repo = 'time-zone-sync-planner'
 
 export default defineConfig({
   plugins: [vue()],
